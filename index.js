f2bed49877ca3eb4bccf107a8d9aa25c19c42387
Thiago-metzlll <thiagostopassola@gmail.com>
2025-05-06 21:49:44 -0300

    ..attt

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-//Aqui estamos fazendo o cabeçário usando JS
-//let div: cria a memória, ou seja, a tag, deve ser nesse modelo, usando a biblioteca docment. Entre parêntesis é o tipo da tag (div,h1...) e em laranja, o nome da classe ou id
-let div = document.createElement(div)
-div.className = 'header_info'
+document.addEventListener('DOMContentLoaded',()=> {//Aqui estamos fazendo o cabeçário usando JS e com a variável let (mais comum e que muda)
+    //let div: cria a memória, ou seja, a tag, deve ser nesse modelo, usando a biblioteca docment. Entre parêntesis é o tipo da tag (div,h1...) e em laranja, o nome da classe ou id
+    //appendChild: insere um filho
+    //h5.textContent: inserir um texto
 
-let div = document.createElement(div)
-div.className = 'profile_logo'
-div.appendChild(profile_logo)
+    let div = document.createElement(div)
+    div.className = 'header_info'
 
-let div = document.createElement(h5)
-div.appendChild(occupation)
-h5.textContent = 'Estudante no Senac-FB'
+    let profile_logo = document.createElement(div)
+    div.className = 'profile_logo'
+    div.appendChild(profile_logo)
 
-let div = document.createElement(div)
-div.className = 'buttons_header'
+    let div = document.createElement(h5)
+    div.appendChild(occupation)
+    h5.textContent = 'Estudante no Senac-FB'
+
+    let div = document.createElement(div)
+    div.className = 'buttons_header'
+    div.appendChild(button)
+
+    let div = document.createElement(div)
+    div.className = 'button'
+
+    let div = document.createElement(i)
+    div.className = 'fa-solid fa-house'
+
+    //A primeira id
+    let div = document.createElement(h5)
+    div.id = 'home'
+
+    let fa_brands fa_github = document.createElement(i)
+    div.className = 'fa-brands fa-github'
+
+})
