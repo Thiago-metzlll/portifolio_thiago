ceb73dc3755d55202c4141cf02ddb4793ed73476
Thiago-metzlll <thiagostopassola@gmail.com>
2025-05-06 21:05:39 -0300

    att

---
+++ index.js
@@ -0,0 +1,15 @@
+//Aqui estamos fazendo o cabeçário usando JS
+//let div: cria a memória, ou seja, a tag, deve ser nesse modelo, usando a biblioteca docment. Entre parêntesis é o tipo da tag (div,h1...) e em laranja, o nome da classe ou id
+let div = document.createElement(div)
+div.className = 'header_info'
+
+let div = document.createElement(div)
+div.className = 'profile_logo'
+div.appendChild(profile_logo)
+
+let div = document.createElement(h5)
+div.appendChild(occupation)
+h5.textContent = 'Estudante no Senac-FB'
+
+let div = document.createElement(div)
+div.className = 'buttons_header'
